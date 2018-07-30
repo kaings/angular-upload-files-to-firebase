79738217c97ab539421f5958b47b9cd94be73cce
willys_tjiawi <willys@huubap.com>
2018-07-30 17:04:08 +0800

    setup firebase config, add markup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,33 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import * as firebase from 'firebase';
+import {environment} from '../environments/environment';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+
+  constructor() {}
+
+  ngOnInit() {
+    firebase.initializeApp(environment.firebaseConfig);
+  }
+  /*
   onSelect(event: MSInputMethodContext) {
     const target: HTMLInputElement = <HTMLInputElement>event.target;
     const files: FileList = target.files;
     const file: File = files[0];
+
+    console.log('selected filename: ', file.name);
+    console.log('selected file object: ', file);
+  }
+  */
+
+  /* another way of onSelect() */
+  onSelect(event: any) {
+    const file: File = event.target.files[0]
 
     console.log('selected filename: ', file.name);
     console.log('selected file object: ', file);
